71f9c85d56b114b9ceba2920fc5c7352507bc9ab
Andrew Kirchner <andrewk@tuta.com>
2025-06-23 23:59:31 -0500

    migrate to IPC sockets instead of disposable MPV instances

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-set -uo pipefail
+set -euo pipefail
 cd $(dirname "$0")
 MPV_CONF="$(pwd)/mpv.conf"
-FILE_OR_DIRECTORY="${1:-"$HOME/Videos"}"
+#TODO: multiple monitor support i.e. multiple sockets
+SOCKET="/tmp/wallpapersocket"
+declare -a DEFAULT_PATHS=("$HOME/Videos")
 
 function throw {
 	notify-send "Error:" "$1" -a "HDRpaper error"
 	echo "$1" >&2
 	exit 1
 }
-
 
 # modular option parser
 # fun learning excercise :)
@@ -22,12 +23,14 @@ function parseoptions {
 	local -r GETOPT="$1"
 	#|I have not found a practical use for new lines here,
 	#|so for now it is cleaner and more secure to ban them
+	#|you can still put newlines inbetween your flags and
+	#|stuff to format them as getopt cleans it
 	if [[ "$GETOPT" == *$'\n'* ]]; then
 		throw "Newlines not allowed in options!"
 	fi
 
-	#|replace all quoted text with arbitrary text of the
-	#|same length in order to further simplify regex
+	#|replace all quoted text with arbitrary text (crosses)
+	#|of the same length in order to further simplify regex
 	local sanitized="$GETOPT"
 	local redaction
 	local quote
@@ -40,6 +43,7 @@ function parseoptions {
 	if grep -Pq -- "(-+[a-z]+).*\1" <<< "$SHORTLONG"; then
 		throw "No duplicate options!"
 	fi
+
 	#|iterate through both boolean flags and key value
 	#|options and assign them to their respective array/map
 	local option
@@ -55,118 +59,119 @@ function parseoptions {
 		fi
 		# I am an option with an argument
 		key="${option%% *}"
-		#|use substring indices to replace crosses from
-		#|earlier with the option
+		#|use substring indices to replenish crosses from
+		#|earlier with the user's option value
 		uptokey=${sanitized%%"$option"*}
 		keyvalueindex=(${#uptokey}+${#key}+2)
+		# replenish sanitizing and shave off quotes
 		value="${GETOPT:$keyvalueindex:(${#option}-${#key}-3)}"
 		value="${value//"'\\''"/"'"}"
 		OPTARGMAP["$key"]="$value"
 	done < <(grep -Po -- "-+[a-z]+(?: X+)?" <<< "$SHORTLONG")
 
+	# do much of the same tricks for positional arguments
 	local argument
 	local -i positional_index
 	local -i positional_length
 	local value
 	while read -r argument; do
+		#|grep outputs a number for where the match is in the string
+		#|and then the match of the crosses delimited by a colon
 		positional_index="${argument%%:*}"
 		argument="${argument##*:}"
 		positional_length="${#argument}"
 		value="${GETOPT:$positional_index+1:$positional_length-2}"
-		echo "$value"
 		value="${value//"'\\''"/"'"}"
 		POSITIONALS+=("$value")
 	done < <(grep -Pob -- "X+(?!.*? --(?:$| ))" <<< "$sanitized")
 }
 
-# parseoptions "$(getopt -o abcx:y:z: -l long,sort: -- "$@")"
-# echo "${FLAGS[@]}"
-# echo "----"
-# echo "keys:${!OPTARGMAP[@]}|values:${OPTARGMAP[@]}"
-# echo "----"
-# echo "${POSITIONALS[@]}"
-
-function playsinglefile {
-	local media_file="$1"
-	if ! [[ $(file -b --mime-type "$media_file") =~ ^(image|video) ]]; then
-		throw "$media_file is not an image or video!"
-	fi
-	if pkill -f "mpv*.--title=wallpaper-mpv"; then
-		echo "Closed previous instance of this script for single file"
-	fi
-	#TODO: using loop and start/end parameters seems to not
-	#be supported for some reason
-	mpv --title=wallpaper-mpv --include="$MPV_CONF" --profile=hdr \
-		--loop=inf \
-		--image-display-duration=inf \
-		"$media_file"
+function plaympv {
+	local wallpaper="$1"
+	# use symlink to avoid json parsing/jq dependency
+	local socat_symlink=$(mktemp -u)
+	ln -s "$wallpaper" "$socat_symlink"
+	socat - "$SOCKET"\
+	<<< "{ \"command\": [\"loadfile\", \"$socat_symlink\", \"replace\"] }"\
+	>/dev/null
+	unlink "$socat_symlink"
 }
 
-function timempv {
-	local media_file="$1"
-	# enum "image" or "video"
-	local filetype=$(file -b --mime-type $media_file | cut -d / -f 1)
-
-	if [[ "$filetype"=="image" ]]; then
-		echo ""
-	fi
-	# adjust when a video starts or ends based on its filename
-	# ex: 10video20.mp4 will start 10 seconds into the video and end 20 seconds in
-	local filename=$(basename $media_file)
+#TODO: add this back in
+function timevideo {
+	local videoname=$(basename "$1")
 	local parameters=""
-	local start_time
-	local end_time
-	if start_time=$(grep -Po '^\d{1,6}' <<< "$filename"); then
+	local -i start_time
+	local -i end_time
+	if start_time=$(grep -Po '^\d{1,6}' <<< "$videoname"); then
 		parameters+="--start=$start_time "
 	fi
-	if end_time=$(grep -Po '\d{1,6}(?=\.[^.]+$)' <<< "$filename");  then
+	if end_time=$(grep -Po '\d{1,6}(?=\.[^.]+$)' <<< "$videoname");  then
 		parameters+="--end=$end_time "
 	fi
 	echo "$parameters"
 }
 
-if [[ -f $FILE_OR_DIRECTORY ]]; then
-	playsinglefile $FILE_OR_DIRECTORY
-	exit 0
-fi
-if [[ ! -d $FILE_OR_DIRECTORY ]]; then
-	throw "$FILE_OR_DIRECTORY is not a file or directory!"
-fi
 
-if pkill -f "mpv*.--title=wallpaper-mpv"; then
-	echo "Closed previous instance of this script"
+SHORTOPTIONS="hvqrs:i:"
+LONGOPTIONS="help,version,quit,replay,sort:,image-display-duration:"
+parseoptions "$(getopt -o "$SHORTOPTIONS" -l "$LONGOPTIONS" -- "$@")"
+
+if [[ ! "${POSITIONALS[@]}" ]]; then
+	POSITIONALS=("${DEFAULT_PATHS[@]}")
 fi
 
-
-declare -a wallpapers
-# split by null byte to account for spaces/emojis/newlines
-mapfile -d '' wallpapers < <(find $FILE_OR_DIRECTORY -type f -print0)
-# remove non media files
-for fileindex in "${!wallpapers[@]}"; do
-	if [[ $(file -b --mime-type "${wallpapers[$fileindex]}") =~ ^(image|video) ]];
-		then continue;
+declare -a WALLPAPERS
+for media_path in "${POSITIONALS[@]}"; do
+	if [[ ! -r "$media_path" ]]; then
+		throw "\"$media_path\" is not a file or directory!"
 	fi
-	unset "wallpapers[$fileindex]"
-done
-
-declare -ir RANGE=(${#wallpapers[@]}-1)
-declare -i last_index=-1
-declare -i random
-while true; do
-	random=$(shuf -i 0-$RANGE -n 1)
-	if [[ $random -eq $last_index ]]; then
+	if [[ -d "$media_path" ]]; then
+		while IFS= read -r -d $'\0' item; do
+			if [[ $(file -ib "$item") =~ ^(image|video) ]]; then
+				WALLPAPERS+=("$item")
+			fi
+		done < <(find "$media_path" -type f -print0)
 		continue
 	fi
-	last_index=$random
-
-	media_file="${wallpapers[$random]}"
-	filename_parameters="$(timempv "$media_file")"
-	echo "MPV is playing $media_file"
-	mpv --title=wallpaper-mpv --include="$(pwd)/mpv.conf" --profile=hdr $filename_parameters"$media_file"
-	if [[ $? == 4 ]]; then
-		echo "MPV has been pkilled, exiting"
-		exit 0
+	if [[ $(file -ib "$media_path") =~ ^(image|video) ]]; then
+		WALLPAPERS+=("$media_path")
 	fi
-	# prevent crash after a few hours of buildup
+done
+if [[ ${#WALLPAPERS[@]} == 0 ]]; then
+	throw "No wallpapers supplied from paths!"
+fi
+if [[ ${#WALLPAPERS[@]} == 1 ]]; then
+	echo "Playing single file forever..."
+	mpv --title=wallpaper-mpv --include="$MPV_CONF" \
+	--loop=inf --image-display-duration=inf \
+	-- "${POSITIONALS[0]}"
+	exit 1
+fi
+
+
+if pkill -f "mpv --title=wallpaper-mpv"; then
+	echo "Closed previous instance of this script!"
+fi
+rm -f "$SOCKET"
+mpv --title=wallpaper-mpv --input-ipc-server="$SOCKET" --include="$MPV_CONF" &
+# > /dev/null 2>&1
+while [[ ! -S "$SOCKET" ]]; do
 	sleep 1
 done
+
+declare -ir RANGE=(${#WALLPAPERS[@]}-1)
+declare -i random=$(shuf -i 0-$RANGE -n 1)
+declare -i last_index=$random
+echo "MPV is playing ${WALLPAPERS[$random]}"
+plaympv "${WALLPAPERS[$random]}"
+while read -r event; do
+	if [[ "$event" != *"idle"* ]]; then continue; fi
+	random=last_index
+	while [[ $random == $last_index ]]; do
+		random=$(shuf -i 0-$RANGE -n 1)
+	done
+	echo "MPV is playing ${WALLPAPERS[$random]}"
+	plaympv "${WALLPAPERS[$random]}"
+done < <(socat - "$SOCKET")
+echo "MPV and program have closed."
